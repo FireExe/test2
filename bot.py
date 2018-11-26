766cfde30f3db56a04ec0a699ed937b8a1671c63
FireExe <45369362+FireExe@users.noreply.github.com>
2018-11-26 20:28:13 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,25 +1,17 @@
 import discord
+from discord.ext import commands
 
-client = discord.Client()
+bot = commands.Bot(command_prefix='$')
 
-@client.event
-async def on_message(message):
-    # we do not want the bot to reply to itself
-    if message.author == client.user:
-        return
-
-    if message.content.startswith('!hello'):
-        msg = 'Hello {0.author.mention}'.format(message)
-        await client.send_message(message.channel, msg)
-
-@client.event
+@bot.event
 async def on_ready():
     print('Logged in as')
-    print(client.user.name)
-    print(client.user.id)
+    print(bot.user.name)
+    print(bot.user.id)
     print('------')
 
+@bot.command()
+async def greet(ctx):
+    await ctx.send(":smiley: :wave: Hello, there!")
 
-    
-    
-client.run(TOKEN)
+bot.run(TOKEN)
