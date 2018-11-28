216b2ad21df4e016f3a0e5d7315e82c0c1a2ae67
FireExe <45369362+FireExe@users.noreply.github.com>
2018-11-28 20:16:55 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -77,7 +77,7 @@ async def assign(ctx, left: str):
         role = discord.utils.get(ctx.guild.roles, name = left)
         user = ctx.message.author
         if left  == "Nopartnerpings":
-          await ctx.send("You now have "+left+" {user}")
+          await ctx.send("You now have "+left+" {0}".format(round(bot.latency, 1))
           print(left)
 
 client.run(TOKEN)
