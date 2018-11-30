6fcd4e1e2587a923bf82c1c165b2791d847bf60a
FireExe <45369362+FireExe@users.noreply.github.com>
2018-11-30 16:47:57 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -95,7 +95,7 @@ async def unassign(ctx, left: str):
 @client.event
 async def on_member_join(member):
     channel = discord.utils.get(member.guild.channels, name="welcome")
-    role = discord.utils.get(server.roles, name="Nopartnerpings")
+    role = discord.utils.get(member.guild.roles, name="Nopartnerpings")
     await client.send(channel, "Welcome "+str(member.mention))
     await member.add_roles(role)
 
