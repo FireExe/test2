7463f08d77c488b49d5dfd63e002655001969a20
FireExe <45369362+FireExe@users.noreply.github.com>
2018-12-02 19:49:44 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -101,6 +101,7 @@ async def unassign(ctx, left: str):
 @client.command(pass_content=True)
 async def kick(ctx, userName: discord.User):
         if ctx.message.author.guild_permissions.kick_members:
+         await client.kick(UserName)
          await ctx.send(str(userName.name)+" has been kicked")
         
         
