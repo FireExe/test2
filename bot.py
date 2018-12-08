bf20615570b03f7fd7add14265a1615f195219c5
FireExe <45369362+FireExe@users.noreply.github.com>
2018-12-08 15:03:34 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ client.remove_command("help")
 
 @client.event
 async def on_ready():
-    await client.change_presence(game=discord.Game(name="Elemental Soul|/help"))
+    await client.change_presence(game=discord.Game(status=discord.Status.Online, activity="Elemental Soul|/help"))
     print('Logged in as')
     print(client.user.name)
     print(client.user.id)
