1da29083215e464b91ae6e46a7b5929335695c97
FireExe <45369362+FireExe@users.noreply.github.com>
2018-12-16 22:09:45 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -224,21 +224,7 @@ async def on_message_delete(before):
     await channel.send(""+str(now.hour)+":"+str(now.minute)+":"+str(now.second)+" :x:**"+str(before.author)+"**"+" (ID:"+str(before.author.id)+"'s has been deleted from "+str(channel2.mention)+":", embed=embed)
 
     
-@client.event
-async def on_message_edit(before, after):
-   if before.author != client or before.author != Bot:
-    now = datetime.datetime.now()
-    embed = discord.Embed(
-        colour = discord.Colour.green()
-    )
-    server = before.guild
-    embed.set_author(name=" ")
-    embed.add_field(name="Edited message:", value="**From:**"+before.content,inline=False)
-    embed.add_field(name="Edited message:", value="**To:**"+after.content,inline=False)
-    channel = discord.utils.get(server.channels, name="log")
-    channel2 = discord.utils.get(server.channels, name=str(before.channel))
-    await channel.send(""+str(now.hour)+":"+str(now.minute)+":"+str(now.second)+" :hammer:**"+str(before.author)+"**"+" (ID:"+str(before.author.id)+"'s has been edited their message in "+str(channel2.mention)+":", embed=embed)
- 
+
     
 client.run(TOKEN)
 
