7c7f0214b495bbff321fd6036f46856c46bbb63f
FireExe <45369362+FireExe@users.noreply.github.com>
2018-12-17 17:31:15 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -76,7 +76,7 @@ async def rob(ctx):
          with open("users.json", "w") as f:
           json.dump(users, f)
         else:
-          await ctx.send("You have "+str(60-(users[str(member.id) + "-" + str(server.id)]["Lastrob"]))+" second until you can rob again")
+          await ctx.send("You have "+str(60-(users[str(user.id) + "-" + str(server.id)]["Lastrob"]))+" second until you can rob again")
     
 @client.command()
 async def bal(ctx):
