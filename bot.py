ddffc16cce5d1b747ff3ae4f42ddcfacdcd81e47
FireExe <45369362+FireExe@users.noreply.github.com>
2018-12-17 17:28:00 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,11 +23,11 @@ async def status_task():
    users = json.load(f)
    for member in x:
         await update_data(users, member, server)
-        if  int(users[str(user.id) + "-" + str(server.id)]["Lastrob"]) !=  int(1):
+        if  int(users[str(member.id) + "-" + str(server.id)]["Lastrob"]) !=  int(1):
          users[str(member.id) + "-" + str(server.id)]["Lastrob"] += 1
          with open("users.json", "w") as f:
           json.dump(users, f)
-          if  int(users[str(user.id) + "-" + str(server.id)]["Lastrob"]) ==  int(60):
+          if  int(users[str(member.id) + "-" + str(server.id)]["Lastrob"]) ==  int(60):
            users[str(member.id) + "-" + str(server.id)]["Lastrob"] = 1
            with open("users.json", "w") as f:
             json.dump(users, f)
