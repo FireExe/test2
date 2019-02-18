4ae964623fb32fe4331a67b7a9ca2300d037826d
FireExe <45369362+FireExe@users.noreply.github.com>
2019-02-18 16:59:56 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -18,17 +18,17 @@ async def status_task():
   await asyncio.sleep(1)
   server = discord.utils.get(client.guilds, name='Bot making')
   x = server.members
-  with open("users.json", "r") as f:
+  with open("users.json", "r+") as f:
    users = json.load(f)
    for member in x:
         await update_data(users, member, server)
         if  int(users[str(member.id) + "-" + str(server.id)]["Lastrob"]) !=  int(1) and int(users[str(member.id) + "-" + str(server.id)]["Lastrob"]) != 60:
          users[str(member.id) + "-" + str(server.id)]["Lastrob"] += 1
-         with open("users.json", "w") as f:
+         with open("users.json", "r+") as f:
           json.dump(users, f)
         elif int(users[str(member.id) + "-" + str(server.id)]["Lastrob"]) ==  int(60):
            users[str(member.id) + "-" + str(server.id)]["Lastrob"] = 1
-           with open("users.json", "w") as f:
+           with open("users.json", "r+") as f:
             json.dump(users, f)
 @client.event
 async def on_ready():
@@ -38,7 +38,7 @@ async def on_ready():
     print(client.user.name)
     print(client.user.id)
     print('------')
-    #client.loop.create_task(status_task())
+    client.loop.create_task(status_task())
     
 
 
@@ -63,14 +63,14 @@ async def add_money(users, user, exp, server):
 
 @client.command()
 async def test(ctx):        
-    with open("C:\\Users\\chzba333@gmail.com\\Desktop\\text.txt","w") as file:
+    with open("C:\\Users\\chzba333@gmail.com\\Desktop\\text.txt","r+") as file:
      newFile.write("yeet")
      newFile.close()
      print("done")
           
 @client.command()
 async def bal(ctx):
-   with open("C:\\Users\\chzba333@gmail.com\\Desktop\\text.txt", "r") as f:
+   with open("users.json", "r") as f:
     users = json.load(f)
     user = ctx.message.author
     server = ctx.message.guild
@@ -82,7 +82,7 @@ async def bal(ctx):
 
 @client.event
 async def on_message(message):
-    with open("C:\\Users\\chzba333@gmail.com\\Desktop\\text.txt", "r") as f:
+    with open("users.json", "r+") as f:
         users = json.load(f)
         if message.author.id == client.user.id:
             return
