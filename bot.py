13faa93dee16dcac55d4d4abbb20dca0c9e0f9ec
FireExe <45369362+FireExe@users.noreply.github.com>
2019-03-10 19:38:50 +0000

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,24 +12,7 @@ client.remove_command("help")
 #you can checkthe Discord API Documentaion for more event Functions 
 # here: https://discordapp.com/developers
 os.chdir = (r"C:\Users\Toshiba pc\PycharmProjects\bots")
-async def status_task():
- while True:
-  now = datetime.datetime.now()
-  await asyncio.sleep(1)
-  server = discord.utils.get(client.guilds, name='Bot making')
-  x = server.members
-  with open("users.json", "r+") as f:
-   users = json.load(f)
-   for member in x:
-        await update_data(users, member, server)
-        if  int(users[str(member.id) + "-" + str(server.id)]["Lastrob"]) !=  int(1) and int(users[str(member.id) + "-" + str(server.id)]["Lastrob"]) != 60:
-         users[str(member.id) + "-" + str(server.id)]["Lastrob"] += 1
-         with open("users.json", "r+") as f:
-          json.dump(users, f)
-        elif int(users[str(member.id) + "-" + str(server.id)]["Lastrob"]) ==  int(60):
-           users[str(member.id) + "-" + str(server.id)]["Lastrob"] = 1
-           with open("users.json", "r+") as f:
-            json.dump(users, f)
+
 @client.event
 async def on_ready():
     activity = discord.Game(name="B-tech ES Bot")
