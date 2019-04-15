3983b8da92a0fe15941dbf2188ea07b99d6af015
FireExe <45369362+FireExe@users.noreply.github.com>
2019-04-15 10:01:36 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,7 +7,7 @@ TOKEN = os.environ['token'] # The token is also substituted for security reasons
 
 client = Bot(command_prefix=BOT_PREFIX)
 client.remove_command("help")
-spam = []
+spam = {}
 
 @client.event
 async def on_ready():
@@ -28,17 +28,17 @@ async def on_message(message):
     user = message.author
     server = message.guild
     if not str(user.id) + "-" + str(server.id) in spam:
-     spam[str(user.id) + "-" + str(server.id)][int(1)] = 1
-     spam[str(user.id) + "-" + str(server.id)][2] = message.content
-     spam[str(user.id) + "-" + str(server.id)][3] = "Empty"
-     spam[str(user.id) + "-" + str(server.id)][4] = "Empty"
+     spam[str(user.id) + "-" + str(server.id)]["Spam"] = 1
+     spam[str(user.id) + "-" + str(server.id)]["Spam1"] = message.content
+     spam[str(user.id) + "-" + str(server.id)]["Spam2"] = "Empty"
+     spam[str(user.id) + "-" + str(server.id)]["Spam3"] = "Empty"
     else:
-      spam[str(user.id) + "-" + str(server.id)] = spam[str(user.id) + "-" + str(server.id)] + 1 
-      if spam[str(user.id) + "-" + str(server.id)][3] == "Empty" and message.content == spam[str(user.id) + "-" + str(server.id)][2]:
-          spam[str(user.id) + "-" + str(server.id)][3] = message.content
-      elif spam[str(user.id) + "-" + str(server.id)][4] == "Empty" and message.content == spam[str(user.id) + "-" + str(server.id)][2]:
-          spam[str(user.id) + "-" + str(server.id)][4] = message.content 
-      elif spam[str(user.id) + "-" + str(server.id)][4] != "Empty":
+      spam[str(user.id) + "-" + str(server.id)]["Spam"] = spam[str(user.id) + "-" + str(server.id)]["Spam"] + 1 
+      if spam[str(user.id) + "-" + str(server.id)]["Spam2"] == "Empty" and message.content == spam[str(user.id) + "-" + str(server.id)]["Spam1"]:
+          spam[str(user.id) + "-" + str(server.id)]["Spam2"] = message.content
+      elif spam[str(user.id) + "-" + str(server.id)]["Spam3"] == "Empty" and message.content == spam[str(user.id) + "-" + str(server.id)]["Spam1"]:
+          spam[str(user.id) + "-" + str(server.id)]["Spam3"] = message.content 
+      elif spam[str(user.id) + "-" + str(server.id)]["Spam3"] != "Empty":
         await message.channel.send("Test")          
     if message.content.startswith("https://discord.gg/"):
         if message.author.guild_permissions.kick_members:
